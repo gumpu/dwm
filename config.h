e05030cfe973912973c79890e33fd82fd6ddd878
Frans Slothouber <frans.slothouber@gmail.com>
2020-10-31 15:11:06 +0100

    Cleanup

--- config.h
+++ config.h
@@ -27,6 +27,7 @@ static const Rule rules[] = {
      * WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
+    { "Evince",   NULL,       NULL,       0,            1,           -1 },
     { "Gimp",     NULL,       NULL,       0,            1,           -1 },
     { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
@@ -79,7 +80,7 @@ static Key keys[] = {
     { MODKEY,                       XK_space,  setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-    { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+/*    { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } }, */
     { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
     { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
     { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
